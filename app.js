36c71cae521ca9fef14b03d02c0d1c602b254516
Daniel <dkbaffour777@gmail.com>
2021-05-06 15:10:34 -0700

    update html template

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template')
 const inquirer = require('inquirer')
 
 const promptUser = () => {
@@ -117,19 +119,11 @@ const promptProject = portfolioData  => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData)
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err)
+
+          console.log('Page created! Check out index.html in this directory to see it!')
+        })
     })
-
-// const fs = require('fs')
-// const generatePage = require('./src/page-template')
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length)
-
-// // Assignment Destructuring
-// const [name, github] = profileDataArgs
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// })
