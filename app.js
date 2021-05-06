76d782a03967737b0e8ca0ef43d2c0cf4ca40b33
Daniel <dkbaffour777@gmail.com>
2021-05-06 10:26:36 -0700

    prompt user for more input

--- app.js
+++ app.js
@@ -1,13 +1,135 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    ])
+}
 
-// Assignment Destructuring
-const [name, github] = profileDataArgs
+const promptProject = portfolioData  => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `)
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = []
+    }
+
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!')
+              return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: desInput => {
+            if (desInput) {
+              return true;
+            } else {
+              console.log('Please enter the description!')
+              return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('Please enter the link!')
+              return false;
+            }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData)
+        } else {
+          return portfolioData
+        }
+    })
+}
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
+
+// const fs = require('fs')
+// const generatePage = require('./src/page-template')
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length)
+
+// // Assignment Destructuring
+// const [name, github] = profileDataArgs
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
